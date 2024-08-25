209d47d93a7a68ed52fc667c0b63f486901c8656
KkoanPC <Luan.ank7667@gmail.com>
2024-08-25 02:13:26 -0300

    mudando o projeto

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-'use strict';
+'use strict'
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function () {
-    // Alterna a classe 'dark-theme' no elemento body
-    document.body.classList.toggle('dark-theme');
-    
-    // Obtém a classe atual do elemento body
-    var classname = document.body.className;
-    
-    // Verifica se a classe atual é 'dark-theme'
+    document.body.classList.toggle('dark-theme')
+    var classname = document.body.classname;
     if (classname.includes('dark-theme')) {
-        this.textContent = "Light"; // Altera o texto do botão para "Light"
-    } else {
-        this.textContent = "Dark"; // Altera o texto do botão para "Dark"
+        this.textContent = "Dark";
     }
-    
-    // Loga a classe atual no console
-    console.log('current class name: ' + classname);
-});
+    else {
+        this.textContent = "ight";
+    }
+    console.log('current class name:' + classname)
+})
